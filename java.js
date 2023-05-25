8d8cf0270e3fc1840e2c3f409dd0d2428446e617
shilasherafat <110653514+shilasherafat@users.noreply.github.com>
2023-05-24 23:07:06 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,3 +1,24 @@
 <script>
 
+const hourHand = document.querySelector('.hour-hand');
+const minHand = document.querySelector('.min-hand');
+const secondHand = document.querySelector('.second-hand');
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hoursDegrees = ((hours % 12) / 12) * 360 + ((minutes / 60) * 30) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
 </script>
